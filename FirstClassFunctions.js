125eafc5d3f1e970c8c1f573c182bb246c2ff08c
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-06 00:39:41 +0530

    Add details about first class functions in JavaScript. (generated by blackbox.ai )

--- FirstClassFunctions.js
+++ FirstClassFunctions.js
@@ -2,16 +2,43 @@
 
 /*
 
-? Function Statement
+? 1.Function Statement / Function Declaration
     function a(){
         console.log("I am a function");
     }
 
-? Function expression
+? 2.Function expression
     var b = function() {
         console.log("b is called");
     }
 
-! 
+! Difference b/w above two is Hoisting 
+
+? 3.Anonymous Function - function without a name
+    function(){
+
+    }
+
+? 4.Named function expression
+    var b = function xyz() {
+
+    }
+
+    if we try  to call it like this: xyz(); It will throw an error because the variable xyz is not defined in that context. 
+
+    but if we call it like this:
+    function xyz(){
+        console.log(xyz) // output : whole function will be printed
+    }
+
+    ! diff b/w 2 & 4 is one has name and another is anonymous function
+
+? 5.First Class Function
+    The ability of functions to be used as values , passed into other functions as arguments, returned from other functionss in JavaScript.
+
+    ? It is also called first class Citizens
+
+
+
 
 */
