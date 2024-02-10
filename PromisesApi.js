0283afbd2b4a402da5f8b02ec8b3d0d0f6f6ec41
Nikhil26062000 <nikhilsbn06@gmail.com>
2024-02-10 19:02:05 +0530

    PromisesApi , this

---
+++ PromisesApi.js
@@ -0,0 +1,71 @@
+
+//? ------------------ 1.Primise.all() -----------------------
+
+const p1 = new Promise((resolve,reject) => {
+    setTimeout(()=>{
+        resolve("p1 promise success");
+    },3000)
+})
+
+const p2 = new Promise((resolve,reject) => {
+    setTimeout(()=>{
+        resolve("p2 promise success");
+    },1000)
+})
+
+const p3 = new Promise((resolve,reject) => {
+    setTimeout(()=>{
+        resolve("p3 promise success");
+    },2000)
+})
+
+// const p4 = new Promise((resolve,reject) => {
+//     setTimeout(()=>{
+//         reject("p4 promise rejected");
+//     },2000)
+// })
+
+
+Promise.all([p1,p2,p3]).then((result)=>{
+    console.log("Promise.all :");
+    console.log(result);  // ["p1 promise success", "p2 promise success", "p3 promise success"]
+}).catch((error)=>{
+    console.error(error); // if any error is thrown it will come here
+    console.log(error.errors) // this will give array of errors for Promise.any()
+})
+
+// here in abvoe example all promises are resolved so it will wait for all promises to be resolved and then return the array containing all the promises
+
+//* but if any promises are rejected it will return the error message of that promise and not wait for rest of the promises
+
+//? ---------------- Promise.allSettled() ------------------
+
+Promise.allSettled([p1,p2,p3]).then((result) => {
+    console.log("Promise.allSettled :")
+    console.log(result); 
+})
+
+// here in allSettled() also if all the promises are resolved then it will give array containig the value of all the promises.
+
+//* but if any promises are rejected then what it will do is that it will still wait for rest of the promises to be settled whether they are resolve or reject it will wait and after all the promises are settled it will give the result in an array containg the result.
+    // example : suppose p2 get reject then the result will be like  ["p1 promise success",["p2 promise rejected or whatever we write in the reject block will bee the result for p2"],["p3 promise success"]]
+
+//? --------------------- Promise.race() ---------------------
+
+Promise.race([p1,p2,p3]).then((result)=>{
+    console.log("Promise.race :");
+    console.log(result);
+})
+
+// in this case the promise who settled first will be returned as the result . it may be resolved or rejected. "settled means it may be resolve or reject or we can say it maybe success or failure or can say fulfilled or rejected. THESE ARE THE COMMON  LINGO'S USED BY THE INTERVIEWERS"
+
+//? --------------------- Promise.any() ------------------
+
+Promise.any([p1,p2,p3]).then((result) => {
+    console.log("Promise.any :");
+    console.log(result);
+})
+
+// in this case the promise who settled first will be returned as the result but that promise must be resolved . what i meant to say that it will wait for the first promise to be resolved among all the promises. 
+
+//* suppose if p2 was rejected them the output will be p3 becz p2 takes 1 sec and p3 takes 2 sec but p2 is rejected that is why p3 will be in result. And suppose p2 and p3 both reject then p1 will be in rsult as output. And suppose if all the promises are rejected the all the error will be returned as an array or can say "aggregate error" will be returned
---
+++ this.js
@@ -0,0 +1,81 @@
+"use strict";
+
+//* --------------- this keyword in global space----------------
+
+    console.log(this); // the value of this keyword in global space can be different in browser , it can be different in node js . it can be different in in different place where we r running js .
+
+    // in browser : this = window object
+    // in node js : this = global object
+
+//* -------- this keyword in function scope ------------
+
+    function x(){
+        console.log(this)
+    }
+    x(); // in strict mode : output : undefined
+    // in non strict mode : output : window object in browser 
+
+    //! this inside non-strict mode (this substitution) : if the value of this keyword is undefined or null , this keyword will be replaced with global object...
+
+    //! this keyword value depends on how the function is called...
+    x();// undefined
+    // but if i called like this
+    window.x(); // it will be different . output : window object in browser
+
+
+//* ---------------- this keyword in object's method -----------------------------------------
+    const obj = {
+        a:10,
+        x:function(){
+            console.log(this);
+        } //! if function created in object its called method
+    };
+
+    obj.x(); // value of this keyword = obj . because this keyword is inside object obj
+
+//* ------------- call , apply , bind methods --------------------
+    const student1 ={
+        name:"Nikhil",
+        printName:function(){
+            console.log(this.name); // Nikhil
+        }
+
+    }
+
+    const student2 ={
+        name:"Nisha",
+    }
+
+    // if u want to change the value of this keyword which is consoled log in student1. we can do this using call(),apply(),bind()
+
+    student1.printName.call(student2); // Nisha
+
+//* ----------------- this keyword inside arrow function --------------------------------------
+    const objj ={
+        a:10,
+        x:()=>{
+            console.log(this);
+        }
+    }
+    objj.x(); //window object
+
+    // inside arrow function this keyword = value of lexical context.in above example objj is in global space so this = window object for browser and global object for node js
+
+    const obj2 ={
+        a:20,
+        x:function(){
+            const y =()=>{
+                console.log(this);
+            }
+            y() //! see video for better understanding
+        }
+    }
+    obj2.x();
+
+//* -------------- this keyword inside DOM ---------------
+    // see video
+
+
+
+
+
